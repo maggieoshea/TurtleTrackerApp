b5f39234885fb4e33e8e75bb5ef77622abc860b5
Maggie O'Shea <margaret.oshea@duke.edu>
2022-09-28 12:45:06 -0400

    task 6

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -128,4 +128,46 @@ for lineString in line_list:
     date_dict[record_id] = obs_date
     location_dict[record_id] = (obs_lat, obs_lon)
 
+#%%
+
+### Task 6 ###
+#Create a variable pointing to the data file
+file_name="Data/raw/Sara.txt"
+
+#Create a file object from the file 
+file_object = open(file_name, "r")
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Create two empty dictionaries
+date_dict = {}
+location_dict =  {}
+
+
+
+#Pretend we read one line of data from the file
+for lineString in line_list:
+    if lineString[0] in ("#", "u"):
+        continue
+#Split the string into a list of data items
+    lineData = lineString.split(    )
     
+    #Extract items in list into variables 
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData [7]
+    
+    #Omit suspect records
+    if obs_lc in ("1", "2", "3"):
+    
+    #Print the location of sara
+        print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon: {obs_lon} on {obs_date}")
+    
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
