346d4d07e3816762d35b1a5bd6c73b6073bca65c
Maggie O'Shea <mageoshea@gmail.com>
2022-10-03 12:22:27 -0400

    editing task 4b, starting task 7

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -88,6 +88,44 @@ for lineString in line_list:
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon: {obs_lon} on {obs_date}")
 #%%
+### Task 4b ###
+
+##A while loop, combined with using the readline() function (vs readlines()) on our file object, 
+#allows us to process just one line at a time, bypassing the need to load the entire file into memory.
+
+#Create a variable pointing to the data file
+file_name="Data/raw/Sara.txt"
+
+#Create a file object from the file 
+file_object = open(file_name, "r")
+
+#Read content of a file into a list
+line_list = file_object.readline()
+
+
+#Pretend we read one line of data from the file
+while lineString:
+    if lineString.startswith("#"or"u"):
+        continue
+        continue
+#Split the string into a list of data items
+    lineData = lineString.split(    )
+    
+    #Extract items in list into variables 
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData [7]
+    
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon: {obs_lon} on {obs_date}")
+    
+    lineString = file_object.readline()
+    
+file_object.close()
+
+#%%
 
 ### Task 5 ###
 #Create a variable pointing to the data file
@@ -131,6 +169,55 @@ for lineString in line_list:
 #%%
 
 ### Task 6 ###
+
+#Ask the user for a date, specifying the format:
+user_date = "7/3/2003"
+
+#Create a variable pointing to the data file
+file_name="Data/raw/Sara.txt"
+
+#Create a file object from the file 
+file_object = open(file_name, "r")
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Create two empty dictionaries
+date_dict = {}
+location_dict =  {}
+
+
+
+#Pretend we read one line of data from the file
+for lineString in line_list:
+    if lineString[0] in ("#", "u"):
+        continue
+#Split the string into a list of data items
+    lineData = lineString.split(    )
+    
+    #Extract items in list into variables 
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData [7]
+    
+    #Omit suspect records
+    if obs_lc in ("1", "2", "3"):
+    
+    #Print the location of sara
+        print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon: {obs_lon} on {obs_date}")
+    
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
+        
+
+#%%
+
+### Task 7 ###
 
 #Ask the user for a date, specifying the format:
 user_date = "7/3/2003"
